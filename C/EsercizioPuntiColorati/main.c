4476e23d906d13f20ac10f9a5dd873ccffcb3081
dxnty <63673949+dxnty@users.noreply.github.com>
2022-06-25 19:19:12 +0200

    Add files via upload

---
+++ C/EsercizioPuntiColorati/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct puntoColorato {
+    char colore[10];
+    int valX; /* valX -> il valore dell'ascissa in cui si trova il punto  */
+} TipoNodo;
+typedef TipoNodo * TipoLista;
+
+int main() {
+
+}
